4653cf39b2e0ed9afd52e7d0dcb9246b55c97c9c
Sylvain METAYER <contact@sylvainmetayer.fr>
2018-04-09 16:42:23 +0200

    Update process data

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let processus = [
-  { user: 'root', pid: 1, start: '16:17', time: '0:01', command: '/usr/bin/echo test' }
-]
+const processus = require('./processus')
 
 console.log(processus)
---
+++ processus.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { pid: 1, start: '1523284547', echeance: '1523284147', command: '/usr/bin/echo process1' },
+  { pid: 2, start: '1523284447', echeance: '1523284647', command: '/usr/bin/echo process2' },
+  { pid: 1, start: '1523284547', echeance: '1523284549', command: '/usr/bin/echo process3' }
+]
